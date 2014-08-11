2591a1165f02c217eb9d52cb60122678b4dded15
hecomi <info@hecomi.com>
2014-08-12 02:43:19 +0900

    JSON 読み込みサンプルを追加 #1

---
+++ Assets/Scripts/Samples/ReadJsonSample.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReadJsonSample : MonoBehaviour 
+{
+	// file path from 'Assets/StreamingAssets'
+	public string jsonPath = "Samples/sample.json"; 
+
+	void Awake()
+	{
+		var json = Utilities.ReadJSON(jsonPath);
+
+		var title = json["title"].ToString();
+		var hogeFuga = json["hoge"]["fuga"].AsInt;
+		var hogeMoge = json["hoge"]["moge"].AsFloat;
+		var piyo = json["piyo"].AsArray;
+
+		Debug.Log(title);     // string
+		Debug.Log(hogeFuga);  // int
+		Debug.Log(hogeMoge);  // float
+		Debug.Log(piyo);      // SimpleJSON.JSONArray
+
+		for (int i = 0; i < piyo.Count; ++i) {
+			var val = piyo[i].AsInt;
+			Debug.Log(val);   // int
+		}
+	}
+}
---
+++ Assets/Scripts/Utilities/ReadJSON.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public static class Utilities 
+{
+	public static SimpleJSON.JSONNode ReadJSON(string path)
+	{
+		var fullPath = Path.Combine(Application.streamingAssetsPath, path);
+		var fileInfo = new FileInfo(fullPath);
+		using ( var stream = new StreamReader(fileInfo.OpenRead(), System.Text.Encoding.UTF8) ) {
+			try {
+				var jsonStr = stream.ReadToEnd();
+				var json = SimpleJSON.JSON.Parse(jsonStr);
+				return json;
+			} catch (System.Exception e) {
+				Debug.LogError(e.StackTrace);
+			}
+		}
+		return null;
+	}
+}
