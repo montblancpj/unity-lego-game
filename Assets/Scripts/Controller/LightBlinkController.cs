4604d2df4d0c575645ed278f54e53fce56363901
hecomi <info@hecomi.com>
2014-08-17 18:44:32 +0900

    ステージの背景とライトを調整 #6

---
+++ Assets/Scripts/Controller/LightBlinkController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightBlinkController : MonoBehaviour 
+{
+	private Light light_;
+	public KeyCode onOffKey = KeyCode.L;
+
+	void Awake()
+	{
+		light_ = gameObject.GetComponent<Light>();
+	}
+
+	void Update() 
+	{
+		if ( Input.GetKeyDown(onOffKey) ) {
+			light_.enabled = !light_.enabled;
+		}
+	}
+}
